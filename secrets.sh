f2c50fc58828d029107b88debf2427970de8a5a4
vplauzon <vplauzon@vplauzon.com>
2018-10-10 16:21:39 -0400

    .

--- secrets.sh
+++ secrets.sh
@@ -1,9 +1,17 @@
-vault=vpl-shared-vault
+vault=<vault name>
 
 # Dev
 
-az keyvault secret set --vault-name $vault --name dev-shared-infra-ssh-public-key --value 
-az keyvault secret set --vault-name $vault --name dev-shared-infra-dns-prefix --value 
-az keyvault secret set --vault-name $vault --name dev-shared-infra-app-id --value 
-az keyvault secret set --vault-name $vault --name dev-shared-infra-object-id --value 
-az keyvault secret set --vault-name $vault --name dev-shared-infra-password --value 
+az keyvault secret set --vault-name $vault --name dev-shared-infra-ssh-public-key --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-shared-infra-dns-prefix --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-shared-infra-app-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-shared-infra-object-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-shared-infra-password --value <VALUE>
+
+# Prod
+
+az keyvault secret set --vault-name $vault --name prod-shared-infra-ssh-public-key --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-shared-infra-dns-prefix --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-shared-infra-app-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-shared-infra-object-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-shared-infra-password --value <VALUE>

c255974568f202f9566a5e733be18aec561428b8
vplauzon <vplauzon@vplauzon.com>
2019-03-05 10:33:53 -0500

    .

--- determine-colour.sh
+++ determine-colour.sh
@@ -1,6 +1,7 @@
 environment=$1
+requestColour=$2
 
-if [$(request-colour) == 'default']
+if [$requestColour == 'default']
 then
     dns=$(az network dns record-set cname show -g vpl-dns -z vplauzon.com -n main-ip.dev --query "cnameRecord.cname" -o tsv)
 
@@ -21,7 +22,7 @@ then
         fi
     fi
 else
-    colour=$(request-colour)
+    colour=$requestColour
 fi
 
 echo "Colour:  $colour"

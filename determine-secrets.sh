f8e595094126f48e0b02aaeb4f727553e25f742e
vplauzon <vplauzon@vplauzon.com>
2019-03-05 13:26:24 -0500

    .

--- determine-secrets.sh
+++ determine-secrets.sh
@@ -3,21 +3,23 @@
 #   Bind script parameters
 environment=$1
 
+echo "Environment:  $environment"
+
 #   Switch environment
 if [[ $environment=='dev' ]]
 then
-    echo "##vso[task.setvariable variable=aks-ssh-public-key;]$(dev-aks-ssh-public-key)"
-    echo "##vso[task.setvariable variable=aks-dns-prefix;]$(dev-aks-dns-prefix)"
-    echo "##vso[task.setvariable variable=aks-app-id;]$(dev-aks-app-id)"
-    echo "##vso[task.setvariable variable=aks-object-id;]$(dev-aks-object-id)"
-    echo "##vso[task.setvariable variable=aks-password;]$(dev-aks-password)"
+    echo "##vso[task.setvariable variable=aks-ssh-public-key;]$2"
+    echo "##vso[task.setvariable variable=aks-dns-prefix;]$3"
+    echo "##vso[task.setvariable variable=aks-app-id;]$4"
+    echo "##vso[task.setvariable variable=aks-object-id;]$5"
+    echo "##vso[task.setvariable variable=aks-password;]$6"
 fi
 
 if [[ $environment=='prod' ]]
 then
-    echo "##vso[task.setvariable variable=aks-ssh-public-key;]$(prod-aks-ssh-public-key)"
-    echo "##vso[task.setvariable variable=aks-dns-prefix;]$(prod-aks-dns-prefix)"
-    echo "##vso[task.setvariable variable=aks-app-id;]$(prod-aks-app-id)"
-    echo "##vso[task.setvariable variable=aks-object-id;]$(prod-aks-object-id)"
-    echo "##vso[task.setvariable variable=aks-password;]$(prod-aks-password)"
+    echo "##vso[task.setvariable variable=aks-ssh-public-key;]$7"
+    echo "##vso[task.setvariable variable=aks-dns-prefix;]$8"
+    echo "##vso[task.setvariable variable=aks-app-id;]$9"
+    echo "##vso[task.setvariable variable=aks-object-id;]$10"
+    echo "##vso[task.setvariable variable=aks-password;]$11"
 fi

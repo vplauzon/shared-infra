199a3423eeaac18bd06327e5549aed8a150c935a
vplauzon <vplauzon@vplauzon.com>
2019-03-05 08:31:29 -0500

    .

--- secrets.sh
+++ secrets.sh
@@ -2,16 +2,22 @@ vault=<vault name>
 
 # Dev
 
-az keyvault secret set --vault-name $vault --name dev-shared-infra-ssh-public-key --value <VALUE>
-az keyvault secret set --vault-name $vault --name dev-shared-infra-dns-prefix --value <VALUE>
-az keyvault secret set --vault-name $vault --name dev-shared-infra-app-id --value <VALUE>
-az keyvault secret set --vault-name $vault --name dev-shared-infra-object-id --value <VALUE>
-az keyvault secret set --vault-name $vault --name dev-shared-infra-password --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-ip-blue --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-ip-green --value <VALUE>
+
+az keyvault secret set --vault-name $vault --name dev-aks-ssh-public-key --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-aks-dns-prefix --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-aks-app-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-aks-object-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name dev-aks-password --value <VALUE>
 
 # Prod
 
-az keyvault secret set --vault-name $vault --name prod-shared-infra-ssh-public-key --value <VALUE>
-az keyvault secret set --vault-name $vault --name prod-shared-infra-dns-prefix --value <VALUE>
-az keyvault secret set --vault-name $vault --name prod-shared-infra-app-id --value <VALUE>
-az keyvault secret set --vault-name $vault --name prod-shared-infra-object-id --value <VALUE>
-az keyvault secret set --vault-name $vault --name prod-shared-infra-password --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-ip-blue --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-ip-green --value <VALUE>
+
+az keyvault secret set --vault-name $vault --name prod-aks-ssh-public-key --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-aks-dns-prefix --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-aks-app-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-aks-object-id --value <VALUE>
+az keyvault secret set --vault-name $vault --name prod-aks-password --value <VALUE>

1d530261bf3ae8158ad4aca1676a4594f9ac2213
vplauzon <vplauzon@vplauzon.com>
2019-03-05 11:01:22 -0500

    .

--- determine-colour.sh
+++ determine-colour.sh
@@ -1,29 +1,29 @@
+#!/bin/sh
+
 environment=$1
 requestColour=$2
 
 echo "Environment:  $environment"
 echo "Request Colour:  $requestColour"
 
-default="default"
-
-if [$requestColour==$default]
+if [[ $requestColour=='default' ]]
 then
+    echo "Fetch DNS recort set..."
+
     dns=$(az network dns record-set cname show -g vpl-dns -z vplauzon.com -n main-ip.dev --query "cnameRecord.cname" -o tsv)
 
     echo "DNS:  $dns"
 
-    blue=$($dns grep blue)
-    green=$($dns grep green)
-
-    if [-n $blue]
+    if [[ $dns == *"blue"* ]]
     then
         colour='blue'
     else
-        if [-n $green]
+        if [[ $dns == *"green"* ]]
         then
             colour='green'
         else
-            raise error "DNS entry doesn't match a colour:  $dns"
+            echo "DNS entry doesn't match a colour:  $dns" 1>&2
+            exit 42
         fi
     fi
 else

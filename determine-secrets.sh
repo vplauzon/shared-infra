fdb5d30114003c4621fbb9852ab3b9d463708ebc
vplauzon <vplauzon@vplauzon.com>
2019-03-05 13:31:36 -0500

    .

--- determine-secrets.sh
+++ determine-secrets.sh
@@ -5,9 +5,14 @@ environment=$1
 
 echo "Environment:  $environment"
 
+a=$(dev-aks-ssh-public-key)
+echo "Test:  $a"
+
 #   Switch environment
 if [[ $environment=='dev' ]]
 then
+    echo "Set Dev Variables..."
+
     echo "##vso[task.setvariable variable=aks-ssh-public-key;]$2"
     echo "##vso[task.setvariable variable=aks-dns-prefix;]$3"
     echo "##vso[task.setvariable variable=aks-app-id;]$4"
@@ -17,6 +22,8 @@ fi
 
 if [[ $environment=='prod' ]]
 then
+    echo "Set Prod Variables..."
+
     echo "##vso[task.setvariable variable=aks-ssh-public-key;]$7"
     echo "##vso[task.setvariable variable=aks-dns-prefix;]$8"
     echo "##vso[task.setvariable variable=aks-app-id;]$9"

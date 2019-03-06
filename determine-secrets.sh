c1260e21f5331923992b6322dec0791d8027aee9
vplauzon <vplauzon@vplauzon.com>
2019-03-05 19:02:31 -0500

    .

--- determine-secrets.sh
+++ determine-secrets.sh
@@ -6,9 +6,3 @@ echo "##vso[task.setvariable variable=aks-dns-prefix;]$2"
 echo "##vso[task.setvariable variable=aks-app-id;]$3"
 echo "##vso[task.setvariable variable=aks-object-id;]$4"
 echo "##vso[task.setvariable variable=aks-password;]$5"
-
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5

37f25236ecd8310693e0c808271c45da0821cb67
vplauzon <vplauzon@vplauzon.com>
2019-04-08 18:04:10 -0400

    Fix Determine IP script

--- determine-ip.sh
+++ determine-ip.sh
@@ -3,23 +3,23 @@
 ###################################################################################################
 ### Determine the ip address of the main public IP of the AKS cluster
 ###
+### Inputs:
+###     nrg:  Node Resource Group
+###     mainIp:  Name of the main ip within nrg
+###
 ### Outputs:
 ###     ip:  IP address
 
 #   Bind script parameters
 nrg=$1
-colour=$2
-environment=$3
+mainIp=$2
 
 echo "Node Resource Group:  $nrg"
-echo "Colour:  $colour"
-echo "Environment:  $environment"
+echo "Main IP:  $mainIp"
 
 #   Get IP address
 echo "Get IP Address..."
-ipName="main-ip-$colour-$environment"
-echo "IP Name:  $ipName"
-ip=$(az network public-ip show -g $nrg -n $ipName -o tsv --query "ipAddress")
+ip=$(az network public-ip show -g $nrg -n $mainIp -o tsv --query "ipAddress")
 echo "IP Address:  $ip"
 
 #   Pass it as an output variable

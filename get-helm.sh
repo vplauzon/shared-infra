f28bc261e346f802cc2a515247d3a6da738947dd
vplauzon <vplauzon@vplauzon.com>
2019-09-05 17:45:52 -0400

    Clean up after get-helm

--- get-helm.sh
+++ get-helm.sh
@@ -6,6 +6,9 @@
 ### Outputs:
 ###     helm (binary file) is output in XYZ folder
 
+mkdir temp-get-helm
+cd temp-get-helm
+
 echo "Download helm"
 
 wget https://get.helm.sh/helm-v3.0.0-beta.3-linux-amd64.tar.gz
@@ -15,4 +18,8 @@ echo "Extract helm"
 tar --extract --file helm-v3.0.0-beta.3-linux-amd64.tar.gz
 
 echo "Move helm"
-mv linux-amd64/helm .
+mv linux-amd64/helm ..
+
+echo "Clean up"
+cd ..
+rm -r temp-get-helm

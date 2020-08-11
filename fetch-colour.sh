97e5f2f7024e2b0e34b1fdf8284b2e0d426da126
vplauzon <vplauzon@vplauzon.com>
2020-08-11 10:00:02 -0400

    .

---
+++ fetch-colour.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+###################################################################################################
+### Fetch the colour to deploy from resource group tag (deploy-colour)
+###
+### Inputs:
+###     resource group name:  name of the resource group to get the colour tag from
+###
+### Outputs:
+###     colour:  colour of the environment (blue or green)
+
+#   Bind script parameters
+rg=$1
+
+echo "Resource group:  $rg"
+
+colour=$(az group show --name test --query tags.test -o tsv)
+
+echo "Colour:  $colour"
+
+echo "##vso[task.setvariable variable=colour;]$colour"

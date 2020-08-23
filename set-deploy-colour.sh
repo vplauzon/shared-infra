a2e1cff4711a9fa5e9e3b2f54f7abad32ce3ccd8
vplauzon <vplauzon@vplauzon.com>
2020-08-23 09:26:28 -0400

    Change colour / env order

--- set-deploy-colour.sh
+++ set-deploy-colour.sh
@@ -3,13 +3,14 @@
 ###################################################################################################
 ### Fetch the colour to deploy from the DNS resource group tag (deploy-colour)
 ###
-### Outputs:
+### Inputs:
 ###     colour:  colour of the environment (blue or green)
 
-rg=vpl-dns
-
-colour=$(az group show --name $rg --query "tags.deployColour" -o tsv)
+colour=$1
 
 echo "Colour:  $colour"
 
-echo "##vso[task.setvariable variable=colour;]$colour"
+rg=vpl-dns
+
+az group update --name $rg --tags deployColour=$colour
+
